de167945928ac176d58670de752945d4805ce57f
Aymen Hasnain <77123184+aymen0627@users.noreply.github.com>
2023-06-13 22:20:48 -0400

    First Commit
    Basic "arrows" cycling through a singular display

---
+++ main/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_idf_version.h>
+#include <max7219.h>
+
+#ifndef APP_CPU_NUM
+#define APP_CPU_NUM PRO_CPU_NUM
+#endif
+
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(4, 0, 0)
+#define HOST    HSPI_HOST
+#else
+#define HOST    SPI2_HOST
+#endif
+
+#define CASCADE_SIZE 1
+#define MOSI_PIN 23
+#define CS_PIN 5
+#define CLK_PIN 18
+
+//static const uint8_t arrow_sequence[] = {10, 11, 12, 13}; // Arrow symbols index in the symbols array
+ 
+static const uint64_t symbols[] = {
+
+    // 0x3c66666e76663c00, // digits 0-9
+    // 0x7e1818181c181800, 
+    // 0x7e060c3060663c00,
+    // 0x3c66603860663c00,
+    // 0x30307e3234383000,
+    // 0x3c6660603e067e00,
+    // 0x3c66663e06663c00,
+    // 0x1818183030667e00,
+    // 0x3c66663c66663c00,
+    // 0x3c66607c66663c00 ,
+
+    0x383838fe7c381000, // arrow up
+    0x10387cfe38383800, //arrow down
+    0x10307efe7e301000, //arrow right
+    0x1018fcfefc181000 //arrow left
+    
+};
+static const size_t symbols_size = sizeof(symbols) - sizeof(uint64_t) * CASCADE_SIZE;
+
+void task(void *pvParameter)
+{
+
+    spi_bus_config_t cfg = {
+       .mosi_io_num = MOSI_PIN,
+       .miso_io_num = -1,
+       .sclk_io_num = CLK_PIN,
+       .quadwp_io_num = -1,
+       .quadhd_io_num = -1,
+       .max_transfer_sz = 0,
+       .flags = 0
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(HOST, &cfg, 1));
+
+
+    max7219_t dev = {
+       .cascade_size = CASCADE_SIZE,
+       .digits = 0,
+       .mirrored = true
+    };
+    ESP_ERROR_CHECK(max7219_init_desc(&dev, HOST, MAX7219_MAX_CLOCK_SPEED_HZ, CS_PIN));
+    ESP_ERROR_CHECK(max7219_init(&dev));
+
+    size_t offs = 0;
+
+    size_t arrow_offset = 0;
+
+
+    while (1)
+    {
+        for (uint8_t i = 0; i < CASCADE_SIZE; i++)
+        {
+            max7219_draw_image_8x8(&dev, i * 8, (uint8_t *)symbols + offs);
+        }
+        vTaskDelay(pdMS_TO_TICKS(500));
+
+        if (++offs == symbols_size)
+            offs = 0;
+    }
+}
+
+void app_main()
+{
+    xTaskCreatePinnedToCore(task, "task", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL, APP_CPU_NUM);
+}
