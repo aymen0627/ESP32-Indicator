18e79e4fefd62edffb5126ef0455ef16216ca963
Aymen Hasnain <77123184+aymen0627@users.noreply.github.com>
2023-07-01 21:51:39 -0400

    Update main.c
    Updated with hard-code zone logic with accurate arrows being displayed

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include <freertos/task.h>
 #include <esp_idf_version.h>
 #include <max7219.h>
+#include <stdbool.h>
 
 #ifndef APP_CPU_NUM
 #define APP_CPU_NUM PRO_CPU_NUM
@@ -18,68 +19,90 @@
 #define MOSI_PIN 23
 #define CS_PIN 5
 #define CLK_PIN 18
+# define NO_DISPLAY 0x00000000000000000
+# define DIRECTIONS 3
 
-//static const uint8_t arrow_sequence[] = {10, 11, 12, 13}; // Arrow symbols index in the symbols array
- 
+// Define arrow symbols
 static const uint64_t symbols[] = {
-
-    // 0x3c66666e76663c00, // digits 0-9
-    // 0x7e1818181c181800, 
-    // 0x7e060c3060663c00,
-    // 0x3c66603860663c00,
-    // 0x30307e3234383000,
-    // 0x3c6660603e067e00,
-    // 0x3c66663e06663c00,
-    // 0x1818183030667e00,
-    // 0x3c66663c66663c00,
-    // 0x3c66607c66663c00 ,
-
+    // Orientation: main pins to the right
     0x383838fe7c381000, // arrow up
-    0x10387cfe38383800, //arrow down
-    0x10307efe7e301000, //arrow right
-    0x1018fcfefc181000 //arrow left
+    //0x10387cfe38383800, // arrow down
+    0x10307efe7e301000, // arrow right
+    0x1018fcfefc181000, // arrow left
+    0x8142241818244281, // X symbol (No space available)
     
 };
-static const size_t symbols_size = sizeof(symbols) - sizeof(uint64_t) * CASCADE_SIZE;
+//static const size_t symbols_size = sizeof(symbols) / sizeof(symbols[0]);
 
 void task(void *pvParameter)
 {
-
     spi_bus_config_t cfg = {
-       .mosi_io_num = MOSI_PIN,
-       .miso_io_num = -1,
-       .sclk_io_num = CLK_PIN,
-       .quadwp_io_num = -1,
-       .quadhd_io_num = -1,
-       .max_transfer_sz = 0,
-       .flags = 0
+        .mosi_io_num = MOSI_PIN,
+        .miso_io_num = -1,
+        .sclk_io_num = CLK_PIN,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 0,
+        .flags = 0
     };
     ESP_ERROR_CHECK(spi_bus_initialize(HOST, &cfg, 1));
 
-
     max7219_t dev = {
-       .cascade_size = CASCADE_SIZE,
-       .digits = 0,
-       .mirrored = true
+        .cascade_size = CASCADE_SIZE,
+        .digits = 0,
+        .mirrored = true
     };
     ESP_ERROR_CHECK(max7219_init_desc(&dev, HOST, MAX7219_MAX_CLOCK_SPEED_HZ, CS_PIN));
     ESP_ERROR_CHECK(max7219_init(&dev));
 
-    size_t offs = 0;
-
-    size_t arrow_offset = 0;
-
+    size_t arrow_index = 0;
 
     while (1)
-    {
-        for (uint8_t i = 0; i < CASCADE_SIZE; i++)
-        {
-            max7219_draw_image_8x8(&dev, i * 8, (uint8_t *)symbols + offs);
-        }
-        vTaskDelay(pdMS_TO_TICKS(500));
+    {   
+        uint64_t available_zones[3] = {NO_DISPLAY, NO_DISPLAY, NO_DISPLAY};
 
-        if (++offs == symbols_size)
-            offs = 0;
+
+        // Read garage_status.txt file and get information
+        int zone1_count = 32;
+        int zone2_count = 33;
+        int zone3_count = 3;
+        int zone1_capacity = 20;
+        int zone2_capacity = 20;
+        int zone3_capacity = 20;
+        
+        // Check if zone 1 is full
+        bool zone1_available = zone1_count < zone1_capacity;
+        if(zone1_available){
+            available_zones[0] = symbols[1];
+        }
+
+        // Check if zone 2 is full
+        bool zone2_available = zone2_count < zone2_capacity;
+        if(zone2_available){
+            available_zones[1] = symbols[2];
+        }
+
+        // Check if zone 3 is full
+        bool zone3_available = zone3_count < zone3_capacity;
+        if(zone3_available){
+            available_zones[2] = symbols[0];
+        }
+
+        bool garage_full = (!zone1_available) && (!zone2_available) && (!zone3_available);
+        if(garage_full){
+            available_zones[0] = symbols[3];
+            available_zones[1] = symbols[3];
+            available_zones[2] = symbols[3];
+        }
+
+        
+        max7219_draw_image_8x8(&dev, 0, (uint8_t *)&available_zones[arrow_index]);
+        vTaskDelay(pdMS_TO_TICKS(820));
+
+        // Move to the next arrow symbol
+        if (++arrow_index == DIRECTIONS){
+            arrow_index = 0;
+        }
     }
 }
 
